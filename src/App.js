453d3bdf37d321a3230d1371510342cd882c4344
Mercy Asante-Ansong <mercansy@Mercys-MacBook-Pro-2.local>
2020-03-22 22:29:57 -0400

    Router 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Link, Switch, Route } from "react-router-dom";
+
+
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
+    <div className="App">
+    <nav>
+      <h1 className="store-header">Christina's Trinkets</h1>
+      <div className="nav-links">
+        <Link className="some-link" to="/">
+          Home
+        </Link>
+        <Link to="/item-list">Shop</Link>
+      </div>
+    </nav>
+
     <main data-testid='app'>
       <Header />
+      <WelcomePage/>
       <CharacterList/>
     </main>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import CharacterList from "./CharacterList";
 
 export default function SearchForm() {
  
