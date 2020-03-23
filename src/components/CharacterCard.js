435bcb8d5a7172e8682e11a6f22b0c355293ca5d
Mercy Asante-Ansong <mercansy@Mercys-MacBook-Pro-2.local>
2020-03-22 21:59:24 -0400

    Characters on screen

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,32 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+import { Card, CardHeader, CardBody, CardText, Col } from "reactstrap";
+
+// export default function CharacterCard() {
+//   return <span>todo: character</span>;
+// }
+
+const CharacterCard = ({ character }) => {
+  return (
+    // <div className="character-list" key={props.id}>
+    //   <h2>Name: {props.name}</h2>
+    //   <h2>Satus: {props.status}</h2>
+    //   <h2>Species: {props.species}</h2>
+    //   <h2>Gender: {props.gender}</h2>
+    // </div>
+
+    <Col xs="6" md="4" xl="3">
+      <br />
+      <Card>
+        <CardHeader>Name: {character.name}</CardHeader>
+        <CardBody>
+          <CardText>Staus: {character.status}</CardText>
+          <CardText>Species: {character.species}</CardText>
+          <CardText>Gender: {character.gender}</CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+};
+
+export default CharacterCard;
